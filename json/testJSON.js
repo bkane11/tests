6ce316370876d2579ee0655090e505e59f30f37f
bk <ben_kane@1575DP-110903.us.ie.urs>
2014-03-25 17:49:06 -0700

    initial commit

---
+++ json/testJSON.js
@@ -0,0 +1,53 @@
+var json = 
+{
+	  "type": "FeatureCollection",
+	  "features": [
+	    {
+	      "type": "Feature",
+	      "properties": {
+	        'name' : 'first',
+	        'RGB' : '51 120 159',
+	        'elevation1' : '50',
+	        'elevation2' : '60',
+	        'elevation3' : '55'},
+	      "geometry": {
+	        "type": "LineString",
+	        "coordinates": [
+	          [
+	            -53.30566406249999,
+	            77.59884823753002
+	          ],
+	          [
+	            -35.5517578125,
+	            75.59587329063447
+	          ],
+	          [
+	            -35.4638671875,
+	            73.76349715052719
+	          ]
+	        ]
+	      }
+	    },
+	    {
+	      "type": "Feature",
+	      "properties": {
+	      'name' : 'second',
+	        'RGB' : '51 120 159',
+	        'elevation1' : '52',
+	        'elevation2' : '51'},
+	      "geometry": {
+	        "type": "LineString",
+	        "coordinates": [
+	          [
+	            -43.9013671875,
+	            76.9007089258869
+	          ],
+	          [
+	            -32.255859375,
+	            74.0799247270789
+	          ]
+	        ]
+	      }
+	    }
+	  ]
+}
